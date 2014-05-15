f89079a31e6dd91f0977a69d5b44e75932392c82
Daniel Manning <daniel.manning@demandmedia.com>
2014-05-15 12:25:57 -0500

    : a-0 initial commit

---
+++ Source/Game1.cs
@@ -0,0 +1,126 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TrianglePrimitive
+{
+	/// <summary>
+	/// This is the main type for your game
+	/// 
+	/// http://rbwhitaker.wikidot.com/index-and-vertex-buffers
+	/// </summary>
+	public class Game1 : Microsoft.Xna.Framework.Game
+	{
+		GraphicsDeviceManager graphics;
+		SpriteBatch spriteBatch;
+
+		VertexBuffer vertexBuffer;
+
+		IndexBuffer indexBuffer;
+
+
+		BasicEffect basicEffect;
+		Matrix world = Matrix.CreateTranslation(0, 0, 0);
+		Matrix view = Matrix.CreateLookAt(new Vector3(0, 0, 3), new Vector3(0, 0, 0), new Vector3(0, 1, 0));
+		Matrix projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45), 800f / 480f, 0.01f, 100f);
+
+		public Game1()
+		{
+			graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+		}
+
+		/// <summary>
+		/// Allows the game to perform any initialization it needs to before starting to run.
+		/// This is where it can query for any required services and load any non-graphic
+		/// related content.  Calling base.Initialize will enumerate through any components
+		/// and initialize them as well.
+		/// </summary>
+		protected override void Initialize()
+		{
+			base.Initialize();
+		}
+
+		/// <summary>
+		/// LoadContent will be called once per game and is the place to load
+		/// all of your content.
+		/// </summary>
+		protected override void LoadContent()
+		{
+			// Create a new SpriteBatch, which can be used to draw textures.
+			spriteBatch = new SpriteBatch(GraphicsDevice);
+
+			basicEffect = new BasicEffect(GraphicsDevice);
+
+			VertexPositionColor[] vertices = new VertexPositionColor[3];
+			vertices[0] = new VertexPositionColor(new Vector3(0, 1, 0), Color.Red);
+			vertices[1] = new VertexPositionColor(new Vector3(+0.5f, 0, 0), Color.Green);
+			vertices[2] = new VertexPositionColor(new Vector3(-0.5f, 0, 0), Color.Blue);
+
+			short[] indices = new short[3];
+			indices[0] = 0;
+			indices[1] = 1;
+			indices[2] = 2;
+
+
+			vertexBuffer = new VertexBuffer(GraphicsDevice, typeof(VertexPositionColor), 3, BufferUsage.WriteOnly);
+			vertexBuffer.SetData<VertexPositionColor>(vertices);
+
+			indexBuffer = new IndexBuffer(graphics.GraphicsDevice, typeof(short), indices.Length, BufferUsage.WriteOnly);
+			indexBuffer.SetData(indices);
+		}
+
+		/// <summary>
+		/// UnloadContent will be called once per game and is the place to unload
+		/// all content.
+		/// </summary>
+		protected override void UnloadContent()
+		{
+		}
+
+		/// <summary>
+		/// Allows the game to run logic such as updating the world,
+		/// checking for collisions, gathering input, and playing audio.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Update(GameTime gameTime)
+		{
+			// Allows the game to exit
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+				this.Exit();
+
+			base.Update(gameTime);
+		}
+
+		/// <summary>
+		/// This is called when the game should draw itself.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.CornflowerBlue);
+
+			basicEffect.World = world;
+			basicEffect.View = view;
+			basicEffect.Projection = projection;
+			basicEffect.VertexColorEnabled = true;
+
+			GraphicsDevice.SetVertexBuffer(vertexBuffer);
+			GraphicsDevice.Indices = indexBuffer;
+
+			RasterizerState rasterizerState = new RasterizerState();
+			rasterizerState.CullMode = CullMode.None;
+			GraphicsDevice.RasterizerState = rasterizerState;
+
+			foreach (EffectPass pass in basicEffect.CurrentTechnique.Passes)
+			{
+				pass.Apply();
+				GraphicsDevice.DrawIndexedPrimitives(PrimitiveType.TriangleList, 0, 0, 3, 0 , 1);
+			}
+
+			base.Draw(gameTime);
+		}
+	}
+}
